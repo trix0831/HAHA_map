cb17c0ead77014a97822e5647157b9e37eb26575
HubertC1 <bb.chang@gmail.com>
2023-12-26 21:49:29 +0800

    add map

--- src/app/docs/[docId]/page.tsx
+++ src/app/docs/[docId]/page.tsx
@@ -1,32 +1,46 @@
 "use client";
 
 import { useDocument } from "@/hooks/useDocument";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow
+} from "@vis.gl/react-google-maps";
+
 
 function DocPage() {
   const { title, setTitle, content, setContent } = useDocument();
+  const position = {lat: 53.54, lng: 10};
   return (
-    <div className="w-full">
-      <nav className="sticky top-0 flex w-full justify-between p-2 shadow-sm">
-        <input
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-          placeholder="Document Title"
-          className="rounded-lg px-2 py-1 text-slate-700 outline-0 focus:bg-slate-100"
-        />
-      </nav>
+    <APIProvider apiKey="AIzaSyBfhfWnkyt-Ntum-ccYifY2ZCzw9hoF030">
+      {/* <div className="w-full">
+        <nav className="sticky top-0 flex w-full justify-between p-2 shadow-sm">
+          <input
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            placeholder="Document Title"
+            className="rounded-lg px-2 py-1 text-slate-700 outline-0 focus:bg-slate-100"
+          />
+        </nav>
 
-      <section className="w-full px-4 py-4">
-        <textarea
-          value={content || ""}
-          onChange={(e) => {
-            setContent(e.target.value);
-          }}
-          className="h-[80vh] w-full outline-0 "
-        />
-      </section>
-    </div>
+        <section className="w-full px-4 py-4">
+          <textarea
+            value={content || ""}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+            className="h-[80vh] w-full outline-0 "
+          />
+        </section>
+      </div> */}
+      <div style={{height: "100vh"}}>
+        <Map zoom={9} center={position}></Map>
+      </div>
+      </APIProvider>
   );
 }
 
