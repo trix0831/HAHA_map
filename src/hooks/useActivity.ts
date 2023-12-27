6086b6d743078c33aff55f8641aa163876547ce7
HubertC1 <bb.chang@gmail.com>
2023-12-28 02:45:39 +0800

    use activity unfinished

--- src/hooks/useActivity.ts
+++ src/hooks/useActivity.ts
@@ -1,12 +1,27 @@
 
 // import { useParams, useRouter } from "next/navigation";
+// import { useState } from "react"
+// import { useRouter } from "next/navigation"
 
-// export const useActivity = () => {
-//   const { docId } = useParams();
-//   const activityId = Array.isArray(docId) ? docId[0] : docId;
+// export default function useActivity(){
+//     const [scheduleName, setScheduleName] = useState([]);
+//     const [scheduleLocation, setScheduleLocation] = useState([]);
+//     const [members, setMembers] = useState([]);
+//     const [dateStart, setDateStart] = useState(0);
+//     const [dateEnd, setDateEnd] = useState(0);
+//     const [description, setDescription] = useState("");
+//     const [title, setTitle] = useState("");
+//     const [location, setLocation] = useState("");
 
-//   const editDescription =async () => {
-    
-//   }
-
+//     const editMembers(userId:string, activityId:string){
+//         const res = await fetch(`/api/activities/${activityId}`,{
+//             method: "PUT",
+//             headers:{
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+                
+//             }),
+//         })
+//     }
 // }
