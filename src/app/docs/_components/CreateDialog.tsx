55e42ec60d1c41738379be5ebe2a2a4c88419ef1
trix0831 <joseph831hsu@gmail.com>
2023-12-27 16:53:38 +0800

    lint

--- src/app/docs/_components/CreateDialog.tsx
+++ src/app/docs/_components/CreateDialog.tsx
@@ -46,7 +46,7 @@ async function CreateDialog() {
 
             const createData={title:title, description:description, dateStart:dateStart, dateEnd:dateEnd, organizerId:userId}
 
-            const result = await createActivity(createData);
+            // const result = await createActivity(createData);
             
             // if (!result) {
             //   redirect(`${publicEnv.NEXT_PUBLIC_BASE_URL}/docs/${docId}`);
