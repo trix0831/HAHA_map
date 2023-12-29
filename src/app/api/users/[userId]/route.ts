195cb747c0be56bebeee2327fb84ecf5b43b2703
HubertC1 <bb.chang@gmail.com>
2023-12-29 22:45:33 +0800

    comment users/GET to fix lint

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -1,51 +1,51 @@
-import { NextResponse, type NextRequest } from "next/server";
+// import { NextResponse, type NextRequest } from "next/server";
 
-import { eq } from "drizzle-orm";
+// import { eq } from "drizzle-orm";
 
-import { db } from "@/db";
-import { usersToActivitiesTable } from "@/db/schema";
+// import { db } from "@/db";
+// import { usersToActivitiesTable } from "@/db/schema";
 
 
-// GET /api/documents/:userId
-export async function GET(
-  req: NextRequest,
-  {
-    params,
-  }: {
-    params: {
-      userId: string;
-    };
-  },
-) {
-  try {
-    // Get user from session
+// // GET /api/documents/:userId
+// export async function GET(
+//   req: NextRequest,
+//   {
+//     params,
+//   }: {
+//     params: {
+//       userId: string;
+//     };
+//   },
+// ) {
+//   try {
+//     // Get user from session
 
-    // Get the document
-		const activities_temp = await db.query.usersToActivitiesTable.findMany({
-			where: eq(usersToActivitiesTable.userId, params.userId),
-			with: {
-				activity: {
-					columns: {
-						displayId: true,
-						title: true,
-					}
-				}
-			}
-		})
-		const activities:{title: string, displayId: string}[] = [];
-		for(let i = 0; i < activities_temp.length; i++){
-			activities.push(activities_temp[i].activity);
-		}
+//     // Get the document
+// 		const activities_temp = await db.query.usersToActivitiesTable.findMany({
+// 			where: eq(usersToActivitiesTable.userId, params.userId),
+// 			with: {
+// 				activity: {
+// 					columns: {
+// 						displayId: true,
+// 						title: true,
+// 					}
+// 				}
+// 			}
+// 		})
+// 		const activities:{title: string, displayId: string}[] = [];
+// 		for(let i = 0; i < activities_temp.length; i++){
+// 			activities.push(activities_temp[i].activity);
+// 		}
 	
-		return activities;
-    } catch (error) {
-    return NextResponse.json(
-      {
-        error: "Internal Server Error",
-      },
-      {
-        status: 500,
-      },
-    );
-  }
-}
+// 		return activities;
+//     } catch (error) {
+//     return NextResponse.json(
+//       {
+//         error: "Internal Server Error",
+//       },
+//       {
+//         status: 500,
+//       },
+//     );
+//   }
+// }
