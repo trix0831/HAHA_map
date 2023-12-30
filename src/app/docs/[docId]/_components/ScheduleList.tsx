b54c93773f93e421e89148a62714618b5f9b391a
trix0831 <joseph831hsu@gmail.com>
2023-12-30 11:48:48 +0800

    Fix swap button visibility in ScheduleList component

--- src/app/docs/[docId]/_components/ScheduleList.tsx
+++ src/app/docs/[docId]/_components/ScheduleList.tsx
@@ -61,21 +61,21 @@ function ScheduleList({setLoca, name, location}: ScheduleListProps) {
               />
 
               <div className='hover:bg-slate-300 rounded-2xl mr-1' onClick={() => {swap(index)}}>
-                <svg fill="#88c7f7" height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xmlSpace="preserve">
+                {(index != 0) && <svg fill="#88c7f7" height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xmlSpace="preserve">
                   <g>
                     <path d="M24.4,24c-0.9,0-1.8-0.3-2.5-1l-5.8-5.7c-0.1-0.1-0.2-0.1-0.3,0L10.1,23c-1.4,1.4-3.6,1.4-5,0c-0.7-0.7-1-1.6-1-2.5
                     c0-1,0.4-1.8,1-2.5l9.4-9.3c0.9-0.9,2.3-0.9,3.1,0l9.4,9.3c0.7,0.7,1,1.6,1,2.5c0,1-0.4,1.8-1,2.5C26.2,23.7,25.3,24,24.4,24z" />
                   </g>
-                </svg>
+                </svg>}
               </div>
 
               <div className='hover:bg-slate-300 rounded-2xl mr-1' onClick={() => {swap(index+1)}}>
-                <svg fill="#88c7f7" height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xmlSpace="preserve">
+                {(index != location.length - 1) ? (<svg fill="#88c7f7" height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xmlSpace="preserve">
                   <g>
                     <path d="M16,25c-0.6,0-1.1-0.2-1.6-0.6l-9.4-9.3c-0.7-0.7-1-1.6-1-2.5c0-1,0.4-1.8,1-2.5c1.4-1.4,3.6-1.4,5,0l5.8,5.7
                     c0.1,0.1,0.2,0.1,0.3,0l5.8-5.7c1.4-1.4,3.6-1.4,5,0c0.7,0.7,1,1.6,1,2.5c0,1-0.4,1.8-1,2.5l-9.4,9.3C17.1,24.8,16.6,25,16,25z" />
                   </g>
-                </svg>
+                </svg>) : (<div className='w-6 h-6'></div>)}
               </div>
 
             <div className='hover:bg-slate-300 rounded-2xl mr-1'>
