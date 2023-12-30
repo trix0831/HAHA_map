e50304f07f005519b4b4dcdae33ddd2bfdbd7ab2
trix0831 <joseph831hsu@gmail.com>
2023-12-30 12:44:45 +0800

    fix schedule up and down

--- src/app/docs/[docId]/_components/ScheduleList.tsx
+++ src/app/docs/[docId]/_components/ScheduleList.tsx
@@ -75,14 +75,14 @@ function ScheduleList({setLoca, name, location, participateState}: ScheduleListP
                 .join('/')}  sx={{ whiteSpace: 'nowrap' }}
               />
 
-              <div className='hover:bg-slate-300 rounded-2xl mr-1' onClick={() => {
-                if(participateState){
+              <div className={`${(index != 0) ? "hover:bg-slate-300" : ''} rounded-2xl mr-1`} onClick={() => {
+                if(participateState && index !== 0){
                   swap(index);
                 }else{
                   alert("Please first participate the activity !");
                 }
                 }}>
-                <svg fill="#88c7f7" height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xmlSpace="preserve">
+                <svg fill={`${(index != 0) ? "#88c7f7" : "#b7bdb8"}`} height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xmlSpace="preserve">
                   <g>
                     <path d="M24.4,24c-0.9,0-1.8-0.3-2.5-1l-5.8-5.7c-0.1-0.1-0.2-0.1-0.3,0L10.1,23c-1.4,1.4-3.6,1.4-5,0c-0.7-0.7-1-1.6-1-2.5
                     c0-1,0.4-1.8,1-2.5l9.4-9.3c0.9-0.9,2.3-0.9,3.1,0l9.4,9.3c0.7,0.7,1,1.6,1,2.5c0,1-0.4,1.8-1,2.5C26.2,23.7,25.3,24,24.4,24z" />
@@ -90,14 +90,14 @@ function ScheduleList({setLoca, name, location, participateState}: ScheduleListP
                 </svg>
               </div>
 
-              <div className='hover:bg-slate-300 rounded-2xl mr-1' onClick={() => {
-                if(participateState){
+              <div className={`${(index != location.length -1) ? "hover:bg-slate-300" : ''} rounded-2xl mr-1`} onClick={() => {
+                if(participateState && index !== location.length-1){
                   swap(index+1);
                 }else{
                   alert("Please first participate the activity !");
                 }
               }}>
-                <svg fill="#88c7f7" height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xmlSpace="preserve">
+                <svg fill={`${(index != location.length - 1) ? "#88c7f7" : "#b7bdb8"}`} height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xmlSpace="preserve">
                   <g>
                     <path d="M16,25c-0.6,0-1.1-0.2-1.6-0.6l-9.4-9.3c-0.7-0.7-1-1.6-1-2.5c0-1,0.4-1.8,1-2.5c1.4-1.4,3.6-1.4,5,0l5.8,5.7
                     c0.1,0.1,0.2,0.1,0.3,0l5.8-5.7c1.4-1.4,3.6-1.4,5,0c0.7,0.7,1,1.6,1,2.5c0,1-0.4,1.8-1,2.5l-9.4,9.3C17.1,24.8,16.6,25,16,25z" />
