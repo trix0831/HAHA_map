f6499feeb2b31444b83c54171173372b8dfc2438
trix0831 <joseph831hsu@gmail.com>
2023-12-30 11:07:31 +0800

    UI

--- src/app/docs/_components/ActivityList.tsx
+++ src/app/docs/_components/ActivityList.tsx
@@ -28,6 +28,18 @@ export function ActivityList({allActivities, myActivities, activityID} : Activit
 
   const [mine, setMine] = useState<boolean>(true);
   const [text, setText] = useState(''); 
+  const [myButtonText, setMyButtonText] = useState<string>('Only show my activities');
+
+  function handleMyClick() {
+    setMine(!mine);
+
+    if (mine) {
+      setMyButtonText('Show all activities');
+    } else {
+      setMyButtonText('Only show my activities');
+    }
+  }
+
 
   const myActivityIds = myActivities.map(doc => doc.displayId);
 
@@ -49,16 +61,14 @@ export function ActivityList({allActivities, myActivities, activityID} : Activit
             <div className="absolute w-3/12 h-9 mt-5 bg-slate-100 bg-opacity-100 "/>
 
             <button
-              onClick={() => {setMine(!mine)}}
-              className={`flex items-center absolute h-7 w-3/12 mt-3 px-3 text-left text-sm text-slate-500 hover:bg-slate-200 ${mine ? 'bg-slate-100' : 'bg-green-500'}`}
+              onClick={() => {handleMyClick()}}
+              className={`flex items-center absolute h-7 w-3/12 mt-3 px-3 text-left text-sm text-slate-500 hover:bg-slate-200 `}
             >
-              <svg width="22" height="22" className="mt-15" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet">
-                  <path d="M116.46 3.96h-104c-4.42 0-8 3.58-8 8v104c0 4.42 3.58 8 8 8h104c4.42 0 8-3.58 8-8v-104c0-4.42-3.58-8-8-8z" fill="#689f38"></path>
-                  <path d="M110.16 3.96h-98.2a7.555 7.555 0 0 0-7.5 7.5v97.9c-.01 4.14 3.34 7.49 7.48 7.5H110.06c4.14.01 7.49-3.34 7.5-7.48V11.46c.09-4.05-3.13-7.41-7.18-7.5h-.22z" fill="#7cb342"></path>
-                  <path d="M40.16 12.86c0-2.3-1.6-3-10.8-2.7c-7.7.3-11.5 1.2-13.8 4s-2.9 8.5-3 15.3c0 4.8 0 9.3 2.5 9.3c3.4 0 3.4-7.9 6.2-12.3c5.4-8.7 18.9-10.6 18.9-13.6z" opacity=".3" fill="#ffffff"></path>
-                  <path d="M43.26 109.46a8.862 8.862 0 0 1-8.7-6.2l-15.1-45.5c-1.46-4.81 1.26-9.9 6.07-11.36c4.65-1.41 9.59 1.08 11.23 5.66l9.8 29.5l47.1-59.6c3.12-3.95 8.85-4.62 12.8-1.5s4.62 8.85 1.5 12.8l-57.6 72.7a9.086 9.086 0 0 1-7.1 3.5z" fill="#fbf9f9"></path>
+                <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path opacity="0.5" d="M12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22Z" fill="#6d7694"/>
+                  <path d="M16.0303 8.96967C16.3232 9.26256 16.3232 9.73744 16.0303 10.0303L11.0303 15.0303C10.7374 15.3232 10.2626 15.3232 9.96967 15.0303L7.96967 13.0303C7.67678 12.7374 7.67678 12.2626 7.96967 11.9697C8.26256 11.6768 8.73744 11.6768 9.03033 11.9697L10.5 13.4393L14.9697 8.96967C15.2626 8.67678 15.7374 8.67678 16.0303 8.96967Z" fill="#1C274C"/>
               </svg>
-              <p className="ml-3">Only show my activities</p> 
+              <p className="ml-3">{myButtonText}</p> 
             </button>
 
               <div className="absolute w-3/12 h-11 mt-24 py-1 bg-slate-100 bg-opacity-100"/>
@@ -120,12 +130,12 @@ export function ActivityList({allActivities, myActivities, activityID} : Activit
                     </Link>   
 
                     {(myActivityIds.includes(doc.displayId)) && 
-                        <svg width="16" height="16" className="mr-4" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet">
-                            <path d="M116.46 3.96h-104c-4.42 0-8 3.58-8 8v104c0 4.42 3.58 8 8 8h104c4.42 0 8-3.58 8-8v-104c0-4.42-3.58-8-8-8z" fill="#689f38"></path>
-                            <path d="M110.16 3.96h-98.2a7.555 7.555 0 0 0-7.5 7.5v97.9c-.01 4.14 3.34 7.49 7.48 7.5H110.06c4.14.01 7.49-3.34 7.5-7.48V11.46c.09-4.05-3.13-7.41-7.18-7.5h-.22z" fill="#7cb342"></path>
-                            <path d="M40.16 12.86c0-2.3-1.6-3-10.8-2.7c-7.7.3-11.5 1.2-13.8 4s-2.9 8.5-3 15.3c0 4.8 0 9.3 2.5 9.3c3.4 0 3.4-7.9 6.2-12.3c5.4-8.7 18.9-10.6 18.9-13.6z" opacity=".3" fill="#ffffff"></path>
-                            <path d="M43.26 109.46a8.862 8.862 0 0 1-8.7-6.2l-15.1-45.5c-1.46-4.81 1.26-9.9 6.07-11.36c4.65-1.41 9.59 1.08 11.23 5.66l9.8 29.5l47.1-59.6c3.12-3.95 8.85-4.62 12.8-1.5s4.62 8.85 1.5 12.8l-57.6 72.7a9.086 9.086 0 0 1-7.1 3.5z" fill="#fbf9f9"></path>
-                        </svg>}             
+                      <svg width="22" height="22" viewBox="0 0 24 24" 
+                      className="mr-4"
+                      fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path opacity="0.5" d="M12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22Z" fill="#05fa26"/>
+                          <path d="M16.0303 8.96967C16.3232 9.26256 16.3232 9.73744 16.0303 10.0303L11.0303 15.0303C10.7374 15.3232 10.2626 15.3232 9.96967 15.0303L7.96967 13.0303C7.67678 12.7374 7.67678 12.2626 7.96967 11.9697C8.26256 11.6768 8.73744 11.6768 9.03033 11.9697L10.5 13.4393L14.9697 8.96967C15.2626 8.67678 15.7374 8.67678 16.0303 8.96967Z" fill="#1C274C"/>
+                      </svg>}             
                   </div>
                 );}
               else{
@@ -162,12 +172,13 @@ export function ActivityList({allActivities, myActivities, activityID} : Activit
                           </div>
                         </Link>   
     
-                      {(myActivityIds.includes(doc.displayId)) && <svg width="16" height="16" className="mr-4" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet">
-                              <path d="M116.46 3.96h-104c-4.42 0-8 3.58-8 8v104c0 4.42 3.58 8 8 8h104c4.42 0 8-3.58 8-8v-104c0-4.42-3.58-8-8-8z" fill="#689f38"></path>
-                              <path d="M110.16 3.96h-98.2a7.555 7.555 0 0 0-7.5 7.5v97.9c-.01 4.14 3.34 7.49 7.48 7.5H110.06c4.14.01 7.49-3.34 7.5-7.48V11.46c.09-4.05-3.13-7.41-7.18-7.5h-.22z" fill="#7cb342"></path>
-                              <path d="M40.16 12.86c0-2.3-1.6-3-10.8-2.7c-7.7.3-11.5 1.2-13.8 4s-2.9 8.5-3 15.3c0 4.8 0 9.3 2.5 9.3c3.4 0 3.4-7.9 6.2-12.3c5.4-8.7 18.9-10.6 18.9-13.6z" opacity=".3" fill="#ffffff"></path>
-                              <path d="M43.26 109.46a8.862 8.862 0 0 1-8.7-6.2l-15.1-45.5c-1.46-4.81 1.26-9.9 6.07-11.36c4.65-1.41 9.59 1.08 11.23 5.66l9.8 29.5l47.1-59.6c3.12-3.95 8.85-4.62 12.8-1.5s4.62 8.85 1.5 12.8l-57.6 72.7a9.086 9.086 0 0 1-7.1 3.5z" fill="#fbf9f9"></path>
-                          </svg>}             
+                      {(myActivityIds.includes(doc.displayId)) && 
+                      <svg width="22" height="22" viewBox="0 0 24 24" 
+                      className="mr-4"
+                      fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path opacity="0.5" d="M12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22Z" fill="#05fa26"/>
+                          <path d="M16.0303 8.96967C16.3232 9.26256 16.3232 9.73744 16.0303 10.0303L11.0303 15.0303C10.7374 15.3232 10.2626 15.3232 9.96967 15.0303L7.96967 13.0303C7.67678 12.7374 7.67678 12.2626 7.96967 11.9697C8.26256 11.6768 8.73744 11.6768 9.03033 11.9697L10.5 13.4393L14.9697 8.96967C15.2626 8.67678 15.7374 8.67678 16.0303 8.96967Z" fill="#1C274C"/>
+                      </svg>}             
                     </div>
                     }
                   </>
